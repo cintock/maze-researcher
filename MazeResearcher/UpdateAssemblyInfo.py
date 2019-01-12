2554ef07adab1f4093c73c3d916d5d63c7597478
Vadim Nizamov <cintock@gmail.com>
2019-01-12 22:12:02 +0500

    Поправил скрипт сборки. Также закоммитил versioninfo.txt с заглушкой версии, чтобы можно было и без сборочного скрипта собрать.

--- MazeResearcher/UpdateAssemblyInfo.py
+++ MazeResearcher/UpdateAssemblyInfo.py
@@ -5,40 +5,49 @@ ASSEMBLY_INFO_FILE = 'Properties\AssemblyInfo.cs'
 VERSION_INFO_FILE = 'versioninfo.txt'
 
 
-def cd_script_dir():
+def current_dir_set_script_dir():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
-def get_version_file():
+def get_version_from_file():
+    four_number_ver = None
+
     with open(VERSION_INFO_FILE, 'r') as version_file:
         version_string = version_file.read()
 
     ind = version_string.find('-')
 
-    only_ver = version_string[:ind]
+    if ind > 0:
+        only_ver = version_string[:ind]
 
-    return only_ver
+        if re.match(r'[0-z]\.[0-z]\.[0-z]\.[0-z]$', only_ver) is not None:
+            four_number_ver = only_ver
+
+    return four_number_ver
 
 
-def main():
-    cd_script_dir()
-
-    os.system('git describe --long --dirty --always > {0}'.format(VERSION_INFO_FILE))
-
-    print('git command complete')
-
-    version = get_version_file()
-
-    with open(ASSEMBLY_INFO_FILE, 'r') as assembly:
-        assembly_data = assembly.read()
+def update_assembly(version):
+    with open(ASSEMBLY_INFO_FILE, 'r') as assembly_file:
+        assembly_data = assembly_file.read()
 
     updated_assembly = re.sub(r'(\[\s*assembly\s*:\s*AssemblyVersion\s*\(\s*\")([^\"]*)(\"\s*\)\s*\])',
                               r'\g<1>{0}\g<3>'.format(version),
                               assembly_data)
 
     if updated_assembly != assembly_data:
-        with open(ASSEMBLY_INFO_FILE, 'w') as assembly:
-            assembly.write(updated_assembly)
+        with open(ASSEMBLY_INFO_FILE, 'w') as assembly_file:
+            assembly_file.write(updated_assembly)
+
+
+def main():
+    current_dir_set_script_dir()
+
+    os.system('git1 describe --long --dirty > "{0}"'.format(VERSION_INFO_FILE))
+
+    version = get_version_from_file()
+
+    if version is not None:
+        update_assembly(version)
 
 
 main()
