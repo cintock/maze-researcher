57342c6847e40e22833e7183ef520c0e102d3ae8
Vadim Nizamov <cintock@gmail.com>
2019-01-21 13:08:11 +0500

    Потокобезопасное логирование.

--- MazeResearcher/Source/UI/LogTextboxAppender.cs
+++ MazeResearcher/Source/UI/LogTextboxAppender.cs
@@ -5,39 +5,75 @@ using System.Text;
 using System.Windows.Forms;
 using log4net.Appender;
 using log4net.Core;
+using System.Threading;
 
 namespace Maze.UI
 {
     public class LogTextboxAppender : AppenderSkeleton
     {
-        public static TextBox LoggingTextbox { get; set; }
+        private static volatile TextBox loggingTextbox;
+
+        public static TextBox LoggingTextbox
+        { 
+            set
+            {
+                loggingTextbox = value;
+            }
+        }
+
+        private static SpinLock lockBuffer = new SpinLock();
 
         private static List<String> buffer = new List<string>();
 
         private System.Threading.Timer flushTimer = new System.Threading.Timer(Flush);
 
-        // todo: сделано в первом приближении, нужно пересмотреть, потокобезопасности нет
-
         protected override void Append(LoggingEvent loggingEvent)
         {
-            if (LoggingTextbox != null)
+            TextBox textbox = loggingTextbox;
+            if (textbox != null)
             {
                 String mes = RenderLoggingEvent(loggingEvent) + Environment.NewLine;
-                buffer.Add(mes);
+                bool lockIsOk = false;
+                try
+                {
+                    lockBuffer.Enter(ref lockIsOk);
+                    buffer.Add(mes);
+                }
+                finally
+                {
+                    if (lockIsOk)
+                    {
+                        lockBuffer.Exit();
+                    }
+                }
                 flushTimer.Change(100, System.Threading.Timeout.Infinite);
             }
         }
 
         private static void Flush(object state)
         {
-            if (LoggingTextbox != null)
+            TextBox textbox = loggingTextbox;
+            if (textbox != null)
             {
-                String all = String.Join("", buffer.ToArray());
-                LoggingTextbox.Invoke((Action)(() =>
+                bool lockIsOk = false;
+                String cachedMessages;
+                try
                 {
-                    LoggingTextbox.AppendText(all);
+                    lockBuffer.Enter(ref lockIsOk);
+                    cachedMessages = String.Join("", buffer.ToArray());
+                    buffer.Clear();
+                }
+                finally
+                {
+                    if (lockIsOk)
+                    {
+                        lockBuffer.Exit();
+                    }
+                }
+                textbox.Invoke((Action)(() =>
+                {
+                    textbox.AppendText(cachedMessages);
                 }));
-                buffer.Clear();
             }
         }
     }
