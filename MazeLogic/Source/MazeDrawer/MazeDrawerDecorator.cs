cd546fbded428e9b0d1823e8d578aa050e47fb36
Vadim Nizamov <cintock@gmail.com>
2019-02-01 13:59:28 +0500

    В первом приближении добавлен декоратор для поворота изображения.

---
+++ MazeLogic/Source/MazeDrawer/MazeDrawerDecorator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze.Logic
+{
+    public class MazeDrawerDecorator : IMazeDrawer
+    {
+        public MazeDrawerDecorator(IMazeDrawer drawer)
+        {
+            mazeDrawer = drawer;
+        }
+
+        public virtual Bitmap Draw(IMazeView maze, MazeClusters clusters = null)
+        {
+            return mazeDrawer.Draw(maze, clusters);
+        }
+
+        public virtual void SetDrawingSettings(MazeDrawingSettings settings)
+        {
+            mazeDrawer.SetDrawingSettings(settings);
+        }
+
+        private IMazeDrawer mazeDrawer;
+    }
+}
---
+++ MazeLogic/Source/MazeDrawer/MazeDrawerRotateDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Maze.Logic
+{
+    public class MazeDrawerRotateDecorator : MazeDrawerDecorator
+    {
+        public MazeDrawerRotateDecorator(IMazeDrawer drawer) : base(drawer)
+        {
+
+        }
+
+        public override Bitmap Draw(IMazeView maze, MazeClusters clusters = null)
+        {
+            Bitmap mazeImage = base.Draw(maze, clusters);
+            mazeImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            return mazeImage;
+        }
+    }
+}
