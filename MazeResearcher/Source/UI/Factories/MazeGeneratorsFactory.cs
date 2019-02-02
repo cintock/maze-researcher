cfe8a1b895afe28a76cc83885af7cdd7f5a15030
Vadim Nizamov <cintock@gmail.com>
2019-02-02 14:43:06 +0500

    Переделал работу с выбором алгоритмов генерации.

---
+++ MazeResearcher/Source/UI/Factories/MazeGeneratorsFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maze.Logic;
+
+namespace Maze.UI
+{
+    internal class MazeGeneratorsFactory
+    {
+        private MazeGeneratorsFactory()
+        {
+
+        }
+
+        private static MazeGeneratorsFactory instance = new MazeGeneratorsFactory();
+
+        public static MazeGeneratorsFactory Instance
+        {
+            get { return instance; }
+        }
+
+        public IMazeGenerator Create(MazeGeneratorsEnum generator)
+        {
+            IMazeGenerator mazeGenerator;
+            switch (generator)
+            {
+                case MazeGeneratorsEnum.EllerModMazeGenerator:
+                    mazeGenerator = new EllerModMazeGenerator();
+                    break;
+
+                case MazeGeneratorsEnum.EmptyMazeGenerator:
+                    mazeGenerator = new EmptyMazeGenerator();
+                    break;
+
+                case MazeGeneratorsEnum.EmptyDummyMazeGenerator:
+                    mazeGenerator = new EmptyDummyMazeGenerator();
+                    break;
+
+                case MazeGeneratorsEnum.RandomMazeGenerator:
+                    mazeGenerator = new RandomMazeGenerator();
+                    break;
+
+                default:
+                    throw new ArgumentException(
+                        "Не предусмотрено создание генератора лабиринта этого типа");
+            }
+            return mazeGenerator;
+        }
+    }
+}
