7714e37a078fd3e63b14743af599a57aaa463503
Vadim Nizamov <cintock@gmail.com>
2019-02-05 22:13:40 +0500

    В первом приближении сделан WPF интерфейс пользователя.

---
+++ MazeResearcherWpf/BitmapImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MazeResearcherWpf
+{
+    // todo: про этот момент надо основательно подумать
+    internal class BitmapImageConverter 
+    {
+        Bitmap image;
+        public BitmapImageConverter(Bitmap bitmap)
+        {
+            image = bitmap;
+        }
+
+        public BitmapImage Image()
+        {
+            BitmapImage convertedImage = new BitmapImage();
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                image.Save(memoryStream, ImageFormat.Bmp);
+                convertedImage.BeginInit();
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                convertedImage.StreamSource = memoryStream;
+                convertedImage.CacheOption = BitmapCacheOption.OnLoad;
+                convertedImage.EndInit();
+            }
+            return convertedImage;
+        }
+    }
+}
---
+++ MazeResearcherWpf/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Maze.Logic;
+
+namespace MazeResearcherWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IMazeView maze;
+        private Bitmap mazeBitmap;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateMaze(object sender, RoutedEventArgs e)
+        {
+            int rowCount = (int)rowSlider.Value;
+            int colCount = (int)columnSlider.Value;
+
+            IMazeGenerator mazeGenerator = 
+                MazeGeneratorsFactory.Instance.Create(
+                    MazeGeneratorsEnum.EllerModMazeGenerator);
+
+            maze = mazeGenerator.Generate(rowCount, colCount);
+
+            IMazeDrawer drawer =
+                MazeDrawersFactory.Instance.Create(MazeDrawersEnum.StandardMazeDrawer);
+
+            drawer.SetDrawingSettings(MazeDrawingSettings.BlackWhile);
+
+            mazeBitmap = drawer.Draw(maze);
+
+            BitmapSource source = (new BitmapImageConverter(mazeBitmap)).Image();
+
+            mazeImage.Source = source;
+        }
+    }
+}
