69bdcd3cabe9ce2096c55d56b4726fea0d39f553
Vadim Nizamov <cintock@gmail.com>
2019-02-05 22:10:12 +0500

    Настройки по умолчанию BlackWhile для MazeDrawingSettings

--- MazeLogic/Source/MazeDrawingSettings.cs
+++ MazeLogic/Source/MazeDrawingSettings.cs
@@ -4,6 +4,19 @@ namespace Maze.Logic
 {
     public class MazeDrawingSettings
     {
+        public static MazeDrawingSettings BlackWhile {
+            get
+            {
+                MazeDrawingSettings settings = new MazeDrawingSettings();
+                settings.CellWidth = 10;
+                settings.CellHeight = 10;
+                settings.SideColor = Color.Black;
+                settings.BackgroundColor = Color.White;
+                settings.BorderColor = Color.Black;
+                return settings;
+            }
+        }
+
         public int CellWidth { get; set; }
         public int CellHeight { get; set; }
         public Color SideColor { get; set; }
